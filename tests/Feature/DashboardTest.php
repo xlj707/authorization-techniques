55e94b6869d8a8a6f10cc4086aaa95c3ca1d4a3f
xlj707 <xlj707@gmail.com>
2023-12-27 14:37:13 -0600

    changes

---
+++ tests/Feature/DashboardTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class DashboardTest extends TestCase
+{
+    /**@test**/
+   public function it_shows_the_dashboard_page_to_authenticated_users(){
+    
+   }
+}
